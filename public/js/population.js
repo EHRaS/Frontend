50da2246d83556ee61e014bf943d4c1ce3c2c44b
Jack Kingsman <jack.kingsman@gmail.com>
2017-04-04 23:57:04 -0700

    Linting and collapsible init

--- public/js/population.js
+++ public/js/population.js
@@ -1,17 +1,24 @@
+/* globals Materialize, Handlebars, globalDataObj */
+
 function populatePage() {
     "use strict";
-    globalDataObj["lname"] = "Wick";
+    globalDataObj.lname = "Wick";
 
     // Compile Handlebars template
     var source = $("#toCompile").html();
     var template = Handlebars.compile(source);
     var compiled = template(globalDataObj);
     $("#compiledContainer").html(compiled);
+
     Materialize.updateTextFields(); // Ensures labels are properly set to active or inactive
+    $('.collapsible').collapsible(); // update collapsible objects after render
 }
-function clearPage(){
-      $('.dataEntry').each(function(){
+
+function clearPage() {
+    "use strict";
+    $('.dataEntry').each(function() {
         $(this).val("");
-      });
-      globalDataObj = {};
+    });
+    
+    globalDataObj = {};
 }
