816809f7a8a27260b583f01ce4b78e9444cd19ee
Jack Kingsman <jack.kingsman@gmail.com>
2017-04-04 17:45:42 -0700

    Add some more todos

--- public/js/population.js
+++ public/js/population.js
@@ -2,3 +2,7 @@ function populatePage() {
     "use strict";
     // TODO: load page with stuff from the now-populated globalDataObj
 }
+
+function clearpage(){
+    // TODO: clear all data here; ready for new load into globalDataObj and call of populatePage
+}
