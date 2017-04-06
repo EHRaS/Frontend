b3c54c855716aaed6c736f35d0091c23861f1abd
Jack Kingsman <jack.kingsman@gmail.com>
2017-04-05 17:56:10 -0700

    Embed NFC listener registration in try/catch

--- public/js/tag.js
+++ public/js/tag.js
@@ -47,27 +47,31 @@ function emptyTagDetected(tagObj) {
         });
 }
 
-navigator.mozNfc.ontagfound = function(event) {
-    "use strict";
-    var tag = event.tag;
+try {
+    navigator.mozNfc.ontagfound = function(event) {
+        "use strict";
+        var tag = event.tag;
 
-    tag.readNDEF().then(records => { //is records a variable?
-        //type will be raw unsigned byte array (Uint8Array)
-        var cryptedUUID = '';
+        tag.readNDEF().then(records => { //is records a variable?
+            //type will be raw unsigned byte array (Uint8Array)
+            var cryptedUUID = '';
 
-        //read tag as an array of MozNDEFRecords & concatenate their payloads (if larger than one record)
-        if (records.size > 1) {
-            for (var i = 0; i < records.size; i += 1) {
-                cryptedUUID += records[i].payload.join('');
+            //read tag as an array of MozNDEFRecords & concatenate their payloads (if larger than one record)
+            if (records.size > 1) {
+                for (var i = 0; i < records.size; i += 1) {
+                    cryptedUUID += records[i].payload.join('');
+                }
+            } else {
+                cryptedUUID = records.payload.join('');
             }
-        } else {
-            cryptedUUID = records.payload.join('');
-        }
 
-        if (cryptedUUID.length === 0) {
-            emptyTagDetected(tag);
-        } else {
-            fullTagDetected(cryptedUUID);
-        }
-    });
-};
+            if (cryptedUUID.length === 0) {
+                emptyTagDetected(tag);
+            } else {
+                fullTagDetected(cryptedUUID);
+            }
+        });
+    };
+} catch (e) {
+    console.log('Browser does not support NFC API; falling back to manual entry.');
+}
