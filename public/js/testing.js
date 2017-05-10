eb6f791125534722a115d12f7080e41b36f87b2e
Jack Kingsman <jack.kingsman@gmail.com>
2017-05-09 23:14:47 -0700

    Add automated field population

--- public/js/testing.js
+++ public/js/testing.js
@@ -162,3 +162,23 @@ function randomString(length) {
 // $(document).ready(function() {
 //     runTests(tests);
 // });
+
+// Demo/debug section
+function debugKepressListener(e) {
+    var evtObj = window.event ? event : e
+    if (evtObj.altKey) {
+        switch (evtObj.keyCode) {
+            case 49: // number 1
+                Materialize.toast('Decrypting tag...', 1000);
+                Materialize.toast('Tag decrypted', 1000);
+                document.activeElement.value = '110ec58a-a0f2-4ac4-8393-c8deadbeef66';
+                break;
+
+            case 50: // number 2
+                document.activeElement.value = 'https://ivmartel.github.io/dwv/demo/stable/viewers/mobile/index.html?input=https%3A%2F%2Fraw.githubusercontent.com%2Fivmartel%2Fdwv%2Fmaster%2Ftests%2Fdata%2F%3Ffile%3Dbbmri-53323851.dcm%26file%3Dbbmri-53323707.dcm%26file%3Dbbmri-53323563.dcm%26file%3Dbbmri-53323419.dcm%26file%3Dbbmri-53323275.dcm%26file%3Dbbmri-53323131.dcm&dwvReplaceMode=void';
+                break;
+        }
+    }
+}
+
+document.onkeydown = debugKepressListener;
